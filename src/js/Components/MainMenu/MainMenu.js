a43b15de70592bc0762fef0a1b4c4ce5cd431bc2
blessssing <betterricherhigher@gmail.com>
2021-10-13 04:56:58 +0300

    update MainMenu

--- src/js/Components/MainMenu/MainMenu.js
+++ src/js/Components/MainMenu/MainMenu.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Menu, Label } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
 
 import "./MainMenu.scss";
 import Home from "@Components/Home";
@@ -9,21 +9,22 @@ import About from "@Components/About";
 import Error404 from "@Components/Error404";
 
 function MainMenu() {
+  const items = [
+    {
+      key: "home",
+      active: true,
+      name: "Home",
+      to: "/",
+      as: Link,
+    },
+    { key: "about", name: "About", to: "/about", as: Link },
+    { key: "test", name: "test", to: "/test", as: Link },
+  ];
+
   return (
     <>
       <Router>
-        <Menu>
-          <Menu.Item as={Link} to="/">
-            Home
-          </Menu.Item>
-          <Menu.Item as={Link} to="/about">
-            About
-          </Menu.Item>
-          <Link to="/link">Link</Link>
-          <Menu.Item as={Link} to="/test">
-            test
-          </Menu.Item>
-        </Menu>
+        <Menu items={items} />
 
         <Switch>
           <Route exact path="/" component={Home} />
