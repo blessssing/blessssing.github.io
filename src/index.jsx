3f93f64725730f5fdf88be0a4aec0e288406b364
blessssing <betterricherhigher@gmail.com>
2021-11-21 02:02:12 +0300

    added theme styles

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "@store";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const Global = createGlobalStyle`
 html {
@@ -22,17 +22,32 @@ html {
 }
 
 body {
-  background-color: #F7F8FA;
+  background-color: ${({ color, theme }) => color || theme.colors.bgBody};
 }
 `;
+
+const theme = {
+  colors: {
+    primary: "#212121",
+    secondary: "#4899AE",
+    white: "white",
+    bgBody: "#F7F8FA",
+  },
+  media: {
+    phone: "(max-width: 500px)",
+    tablet: "(max-width: 768px) and (min-width: 500px)",
+  },
+};
 
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <Global />
-      <Router>
-        <App />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Global />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("app")
