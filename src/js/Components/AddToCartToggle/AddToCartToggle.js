4682052c8294f4a668b91f431abebc75674adc71
blessssing <betterricherhigher@gmail.com>
2021-12-11 16:08:11 +0300

    added AddToCartToggle update styles

---
+++ src/js/Components/AddToCartToggle/AddToCartToggle.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./AddToCartToggle.scss";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { addBookToCart } from "@reducers/cartSlice";
+
+const StyledAddBookBtn = styled.button`
+  margin: 10px 20px;
+  padding: 1.2rem 2.8rem;
+  color: #fff;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 700;
+  text-align: center;
+  text-transform: uppercase;
+  box-shadow: 0 0 40px 40px #3498db inset, 0 0 0 0 #3498db;
+  transition: all 0.25s ease-in-out;
+
+  &:hover {
+    color: #202020;
+    box-shadow: 0 0 5px 0 #3498db inset, 0 0 15px 2px #3498db;
+  }
+`;
+
+const AddBookBtn = (props) => {
+  return <StyledAddBookBtn {...props} />;
+};
+
+const AddToCartToggle = (props) => {
+  console.log("props ", props);
+  const dispatch = useDispatch();
+  const [disabled, setDisabled] = useState(false);
+
+  const ActiveBtn = () => {
+    dispatch(addBookToCart(props.book));
+    setDisabled(true);
+  };
+
+  const DisabledBtn = () => {
+    setDisabled(false);
+  };
+
+  const AddToCart = () => {
+    return (
+      <button className={"add-book-btn add-to-cart-btn"} onClick={ActiveBtn}>
+        Add To Cart
+      </button>
+    );
+  };
+
+  const AddToCartDisabled = () => {
+    return (
+      <button
+        className={"add-book-btn add-to-cart-disabled-btn"}
+        onClick={DisabledBtn}
+      >
+        Remove
+      </button>
+    );
+  };
+
+  return <>{disabled ? <AddToCartDisabled /> : <AddToCart />}</>;
+};
+
+export default AddToCartToggle;
--- src/js/Components/BooksListItem/BooksListItem.js
+++ src/js/Components/BooksListItem/BooksListItem.js
@@ -1,35 +1,9 @@
 import React from "react";
 import "./BooksListItem.scss";
-import { useDispatch } from "react-redux";
 import { addBookToCart } from "@reducers/cartSlice";
-import styled from "styled-components";
-
-const StyledAddBookBtn = styled.button`
-  margin: 10px 20px;
-  padding: 1.2rem 2.8rem;
-  color: #fff;
-  border: 0;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 1rem;
-  font-weight: 700;
-  text-align: center;
-  text-transform: uppercase;
-  box-shadow: 0 0 40px 40px #3498db inset, 0 0 0 0 #3498db;
-  transition: all 0.25s ease-in-out;
-
-  &:hover {
-    color: #202020;
-    box-shadow: 0 0 5px 0 #3498db inset, 0 0 15px 2px #3498db;
-  }
-`;
-
-const AddBookBtn = (props) => {
-  return <StyledAddBookBtn {...props} />;
-};
+import AddToCartToggle from "@Components/AddToCartToggle";
 
 const BooksListItem = (props) => {
-  const dispatch = useDispatch();
   const { title, description, age, author, img, price } = props;
 
   return (
@@ -45,9 +19,7 @@ const BooksListItem = (props) => {
         <div style={{ fontSize: "0.9rem" }}>{age}</div>
         <div>{description}</div>
         <div style={{ fontWeight: 600 }}>Price:&nbsp;$&nbsp;{price}</div>
-        <AddBookBtn onClick={() => dispatch(addBookToCart({ ...props }))}>
-          Add to cart
-        </AddBookBtn>
+        <AddToCartToggle book={props} />
       </div>
     </div>
   );
