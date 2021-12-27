57f644e02127d80ac053422c065da92499f5d0c6
blessssing <betterricherhigher@gmail.com>
2021-12-27 20:10:16 +0300

    update Details, added styles Details.scss

--- src/js/Components/Details/Details.js
+++ src/js/Components/Details/Details.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Details.scss";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import GoBackBtn from "@Components/GoBackBtn";
@@ -6,24 +7,38 @@ import GoBackBtn from "@Components/GoBackBtn";
 const Details = () => {
   const { name } = useParams();
   const allBooks = useSelector((state) => state.books.allBooks);
-  const { age, article, author, description, img, price, title } =
+  const { age, article, author, genre, description, img, price, title } =
     allBooks.find((book) => book.title === name);
 
   return (
-    <div>
+    <article className="book-details-container">
       <GoBackBtn />
-      <div>
-        <div>{title}</div>
-        <div>{age}</div>
-        <div>{article}</div>
-        <div>{author}</div>
-        <div>{description}</div>
-        <div>
+      <div className="book-details">
+        <div className="book-details__img-wrapper">
           <img src={img} alt={img} />
         </div>
-        <div>{price}</div>
+
+        <div className="info-block">
+          <div className="info-block__title">
+            <span className="title">{title}</span>&nbsp;&nbsp;&nbsp;
+            <span className="article">(Art. {article})</span>
+          </div>
+          <div className="info-block__author">
+            <b>Author:</b> {author}
+          </div>
+          <div className="info-block__genre">
+            <b>Genre:</b> {genre}
+          </div>
+          <div className="info-block__age">
+            Age: <small>{age}</small>
+          </div>
+          <div className="info-block__description">
+            Description: {description}
+          </div>
+          <div className="info-block__price">Price: $&nbsp;{price}</div>
+        </div>
       </div>
-    </div>
+    </article>
   );
 };
 
