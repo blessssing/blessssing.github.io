cb6167a044169eabd79f4911ca33c484e18455b1
blessssing <betterricherhigher@gmail.com>
2021-12-27 16:47:56 +0300

    update Cart

--- src/js/Components/Cart/Cart.js
+++ src/js/Components/Cart/Cart.js
@@ -13,48 +13,55 @@ const Cart = () => {
     <>
       <div className={"cart-container cart"}>
         {(cart.length &&
-          cart.map(({ id, article, title, age, author, img, price }, index) => (
-            <div key={article + index} className={"cart__book"}>
-              <div className={"cart__book__wrapper-image"}>
-                <img src={img} alt={img} />
+          cart.map(
+            ({ id, article, title, age, genre, author, img, price }, index) => (
+              <div key={article + index} className={"cart__book"}>
+                <div className={"cart__book__wrapper-image"}>
+                  <img src={img} alt={img} />
+                </div>
+                <div className={"cart__book__info"}>
+                  <div>
+                    <span>(Art. {article})</span>
+                  </div>
+                  <div>
+                    <span>
+                      <b>Title</b>: {title}
+                    </span>
+                  </div>
+                  <div>
+                    <span>
+                      <b>Author:</b> {author}
+                    </span>
+                  </div>
+                  <div>
+                    <span>
+                      <b>Genre:</b> {genre}
+                    </span>
+                  </div>
+                  <div style={{ fontSize: "0.9rem" }}>
+                    <span>
+                      Age group: <small>{age}</small>
+                    </span>
+                  </div>
+                  <div className="price">
+                    <span>
+                      <b>Price: $&nbsp;{price}</b>
+                    </span>
+                  </div>
+                </div>
+                <div className="icon-group">
+                  <Icon
+                    className="move-to-trash-icon"
+                    onClick={() => dispatch(moveToTrash(id))}
+                    name="trash alternate outline"
+                    size="large"
+                    link
+                    color="grey"
+                  />
+                </div>
               </div>
-              <div className={"cart__book__info"}>
-                <div>
-                  <span>(Art. {article})</span>
-                </div>
-                <div>
-                  <span>
-                    <b>Title</b>: {title}
-                  </span>
-                </div>
-                <div>
-                  <span>
-                    <b>Author:</b> {author}
-                  </span>
-                </div>
-                <div>
-                  <span>
-                    <b>Age group:</b> <small>{age}</small>
-                  </span>
-                </div>
-                <div className="price">
-                  <span>
-                    <b>Price: $&nbsp;{price}</b>
-                  </span>
-                </div>
-              </div>
-              <div className="icon-group">
-                <Icon
-                  className="move-to-trash-icon"
-                  onClick={() => dispatch(moveToTrash(id))}
-                  name="trash alternate outline"
-                  size="large"
-                  link
-                  color="grey"
-                />
-              </div>
-            </div>
-          ))) || <h4 style={{ margin: "0 auto" }}>Cart is empty</h4>}
+            )
+          )) || <h4 style={{ margin: "0 auto" }}>Cart is empty</h4>}
       </div>
 
       {(cart.length && <Order />) || null}
