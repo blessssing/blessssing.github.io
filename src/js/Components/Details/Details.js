269e634920884f69f8e646adae3420e1dc1fbfd2
blessssing <betterricherhigher@gmail.com>
2021-12-27 16:13:15 +0300

    update Details

--- src/js/Components/Details/Details.js
+++ src/js/Components/Details/Details.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import GoBackBtn from "@Components/GoBackBtn";
 
 const Details = () => {
-  const params = useParams();
-  console.log("params ", params);
+  const { name } = useParams();
+  const allBooks = useSelector((state) => state.books.allBooks);
+  const { age, article, author, description, img, price, title } =
+    allBooks.find((book) => book.title === name);
 
   return (
     <div>
       <GoBackBtn />
-      Details: {params.name}
+      <div>
+        <div>{title}</div>
+        <div>{age}</div>
+        <div>{article}</div>
+        <div>{author}</div>
+        <div>{description}</div>
+        <div>
+          <img src={img} alt={img} />
+        </div>
+        <div>{price}</div>
+      </div>
     </div>
   );
 };
