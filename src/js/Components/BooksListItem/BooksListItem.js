e8e3b423382fc600f7a0a5e7c5fe394664d06137
blessssing <betterricherhigher@gmail.com>
2023-02-15 05:42:21 +0300

    update BooksListItem

--- src/js/Components/BooksListItem/BooksListItem.js
+++ src/js/Components/BooksListItem/BooksListItem.js
@@ -8,32 +8,30 @@ const BooksListItem = (props) => {
   const navigate = useNavigate();
 
   return (
-    <div className={"books-list-item books-list-item__container"}>
-      <div className="title">
-        <span>
-          <b>{title}</b>
-        </span>
-      </div>
-      <div
-        className={"wrapper-image"}
-        onClick={() => navigate(`/book/${title}`)}
-      >
-        <img src={img} alt={img} />
-      </div>
-      <div className="info">
-        <div className="author">{author}</div>
-        <div className="genre">
-          <b>Жанр:&nbsp;</b>
-          {genre}
+    <div className="books-list-item">
+      <h4 className="title">{title}</h4>
+      <div className={"books-list-item__container"}>
+        <div
+          className={"wrapper-image"}
+          onClick={() => navigate(`/book/${title}`)}
+        >
+          <img src={img} alt={img} />
         </div>
-        <div className="age">
-          <small>{age}</small>
+        <div className="info">
+          <div className="author">{author}</div>
+          <div className="genre">
+            <b>Жанр:&nbsp;</b>
+            {genre}
+          </div>
+          <div className="age">
+            <small>{age}</small>
+          </div>
+          <div className="description">{description}</div>
+          <div className="price">
+            <b>Цена:&nbsp;{price}&nbsp;&#8381;</b>
+          </div>
+          <AddToCartToggle book={props} />
         </div>
-        <div className="description">{description}</div>
-        <div className="price">
-          <b>Цена:&nbsp;{price}&nbsp;&#8381;</b>
-        </div>
-        <AddToCartToggle book={props} />
       </div>
     </div>
   );
