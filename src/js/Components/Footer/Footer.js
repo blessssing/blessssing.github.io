b83844d26a38117ca4b28ce0ac162a800e5a60fd
blessssing <betterricherhigher@gmail.com>
2023-12-29 22:42:40 +0300

    update Footer

--- src/js/Components/Footer/Footer.js
+++ src/js/Components/Footer/Footer.js
@@ -2,30 +2,30 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="social-container">
-        <h5 className="social title">Связаться со мной</h5>
+	return (
+		<footer className="footer">
+			<div className="social-container">
+				<h5 className="social title">Связаться со мной</h5>
 
-        <div className="social">
-          Telegram: @blesssssing&nbsp;&nbsp;&nbsp;
-          <a
-            className="link"
-            href="https://t.me/blesssssing"
-            target="_blank"
-            rel="noreferrer"
-          >
-            https://t.me/blesssssing
-          </a>
-        </div>
-        <div className="social">Skype: Azure</div>
-        <div className="social">Mail: betterricherhigher@gmail.com</div>
-      </div>
-      <div className="rights">
-        <span>Все права защищены 2023.</span>
-      </div>
-    </footer>
-  );
+				<div className="social">
+					Telegram: @blesssssingjs&nbsp;&nbsp;&nbsp;
+					<a
+						className="link"
+						href="https://t.me/blesssssing"
+						target="_blank"
+						rel="noreferrer"
+					>
+						https://t.me/blesssssingjs
+					</a>
+				</div>
+				<div className="social">Skype: Azure</div>
+				<div className="social">Mail: betterricherhigher@gmail.com</div>
+			</div>
+			<div className="rights">
+				<span>Все права защищены 2024.</span>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 export const Global = createGlobalStyle`
 html {
@@ -19,26 +19,26 @@ body {
 `;
 
 export const theme = {
-  colors: {
-    primary: "#212121",
-    secondary: "#4899AE",
-    white: "white",
-    bgBody: "#F7F8FA",
-    goldenrod: "goldenrod",
-    gold: "gold",
-  },
-  media: {
-    phone: "(max-width: 500px)",
-    tablet: "(max-width: 991px)",
-  },
+	colors: {
+		primary: "#212121",
+		secondary: "#4899AE",
+		white: "white",
+		bgBody: "#F7F8FA",
+		goldenrod: "goldenrod",
+		gold: "gold",
+	},
+	media: {
+		phone: "(max-width: 500px)",
+		tablet: "(max-width: 991px)",
+	},
 
-  // $screen-sm: 576px;
-  // $screen-md: 768px;
-  // $screen-lg: 992px;
-  // $screen-xl: 1200px;
-  // === === ===
-  // $screen-xs-max: 575px;
-  // $screen-sm-max: 767px;
-  // $screen-md-max: 991px;
-  // $screen-lg-max: 1199px;
+	// $screen-sm: 576px;
+	// $screen-md: 768px;
+	// $screen-lg: 992px;
+	// $screen-xl: 1200px;
+	// === === ===
+	// $screen-xs-max: 575px;
+	// $screen-sm-max: 767px;
+	// $screen-md-max: 991px;
+	// $screen-lg-max: 1199px;
 };
